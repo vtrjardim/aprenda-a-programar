0c443bf4b0dd28e419e48cb44af112e2e61e0b19
vtrjardim <victorjardga@gmail.com>
2022-08-24 02:38:52 -0300

    commit com desafio opcional feito

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,9 +1,16 @@
-let nome = "Victor"
-let email = "victorjardga@gmail.com"
-let profissao = "arquiteto"
-let idade = 25
-let local = "Toquio"
-console.log(nome+"atua como:"+profissao)
+let nomes = "Victor"
+let emails = "victorjardga@gmail.com"
+let profissaos = "arquiteto"
+let idades = 25
+let locals = "Toquio"
+console.log(nomes+"atua como:"+profissaos)
 console.log("Outras informacoes importantes sobre o user:")
-console.log("Email utilizado: "+email+"idade:"+idade)
-console.log("gostaria de residir em:"+local)
+console.log("Email utilizado: "+emails+"idade:"+idades)
+console.log("gostaria de residir em:"+locals)
+
+var nome = "Pedro"
+var email = "pedrosilva@bol.com"
+var profissao = "Advogado"
+var idade = 34
+var local = "Los Angeles"
+console.log(`Olá, ${nome}! Somos aqui da agência de turismo Viagem dos Sonhos. Estamos te escrevendo este email, pois acabamos de confirmar as compras das passagens aéreas para ${local}. Confirma pra gente alguns dados? A sua idade é ${idade}, você é ${profissao} e o seu email é ${email}? Ficamos no aguardo. Muito obrigado e boa viagem! :D`)
