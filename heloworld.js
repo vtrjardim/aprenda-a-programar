0de1e5e71f07be9f2aee6e64ae2a8df092500f39
vtrjardim <victorjardga@gmail.com>
2022-08-23 21:24:32 -0300

    heloworld

--- heloworld.js
+++ heloworld.js
@@ -1,3 +1,15 @@
 console.log("hello World!")
 console.log("Olá Mundo!")
 console.log("Halo Dunia!")
+//comentário no meio das minhas linhas de código
+//servem para serem lidos por humanos, não pelas máquinas
+
+let minhaVariavel
+let outraVariavel = 1
+minhaVariavel = "ITuring"
+console.log(minhaVariavel)
+console.log(outraVariavel)
+let soma1 = 100
+let soma2 = 300
+console.log("Valor final Gorjeta")
+console.log(soma1+soma2)
