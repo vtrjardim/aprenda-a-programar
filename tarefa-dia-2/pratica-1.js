a4d7c5f2444737f4c7d028c7bb0e6c687d946576
vtrjardim <victorjardga@gmail.com>
2022-08-24 02:09:28 -0300

    testando no pratica-1

--- tarefa-dia-2/pratica-1.js
+++ tarefa-dia-2/pratica-1.js
@@ -1,5 +1,9 @@
 let nome = "Victor"
 let email = "victorjardga@gmail.com"
 let profissao = "arquiteto"
-let idade = "25"
+let idade = 25
 let local = "Toquio"
+console.log(nome+"atua como:"+profissao)
+console.log("Outras informacoes importantes sobre o user:")
+console.log("Email utilizado: "+email+"idade:"+idade)
+console.log("gostaria de residir em:"+local)
