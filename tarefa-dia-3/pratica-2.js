eacb44c31e9b6a0f3699ef6294d0fefb190787a1
vtrjardim <victorjardga@gmail.com>
2022-08-26 16:57:41 -0300

    tarefa dia 3 upload 1

---
+++ tarefa-dia-3/pratica-2.js
@@ -0,0 +1,10 @@
+let multiplicador = 7 
+let i = 1
+let n = 10
+
+while (i <= n) {
+    let resultado = multiplicador * i
+    let texto = `${multiplicador} X ${i} = ${resultado}`
+    console.log(texto)
+    i++
+}
---
+++ tarefa-dia-3/pratica-3.js
@@ -0,0 +1,10 @@
+let n = 1
+let n1 = 16
+
+while (n <= n1) {
+    if (n % 2 == 1)
+        {
+            console.log(n)
+        }
+    n++
+}
